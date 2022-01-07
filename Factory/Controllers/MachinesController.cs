90304d09d8522cfeba635fc75d1086fb6eabc019
Katie Pundt <kzpundt@gmail.com>
2022-01-07 12:56:31 -0800

    add Details route and corresponding Machines/Details View

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -34,6 +34,13 @@ namespace Factory.Controllers
       return RedirectToAction("Index");
     }
 
-    
+    public ActionResult Details(int id)
+    {
+      var thisMachine = _db.Machines
+        .Include(machine => machine.JoinEntities)
+        .ThenInclude(join => join.Engineer)
+        .FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
   }
 }
