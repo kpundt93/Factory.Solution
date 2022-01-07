424d55426858754ac1c2f8f1cf46d8967f64a971
Katie Pundt <kzpundt@gmail.com>
2022-01-07 14:16:00 -0800

    add successful 2nd migration

---
+++ Factory/Migrations/20220107221402_NewProperties.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class NewProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Machines",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Engineers",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Engineers");
+        }
+    }
+}
