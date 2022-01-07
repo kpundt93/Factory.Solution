abb347fea3d555dbc79f750b3c5cf97f9ba5c5b8
Katie Pundt <kzpundt@gmail.com>
2022-01-07 09:19:14 -0800

    add Machine Model

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public Machine()
+    {
+      this.JoinEntities = new HashSet<MachineEngineer>();
+    }
+
+    public int MachineId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public virtual ICollection<MachineEngineer> JoinEntities { get; }
+  }
+}
